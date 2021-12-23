fce6f343b39d3c9ee6db01810340f86438561e41
Airmole <admin@airmole.cn>
2021-12-23 17:59:32 +0800

    version 1.0.0

---
+++ pages/index/index.vue
@@ -0,0 +1,202 @@
+<template>
+	<view class="content">
+		<cu-custom bgColor="bg-gradual-blue">
+			<block slot="content">公寓门禁码</block>
+			<block slot="right">
+				<button @click="go2Setting" class="cu-btn cuIcon margin-lr-xs" style="background-color: unset;">
+					<text class="text-white cuIcon-settingsfill"></text>
+				</button>
+			</block>
+		</cu-custom>
+
+		<view class="qrcode-box">
+			<uqrcode ref="uqrcode" />
+			<view class="margin-top">{{ tips }}</view>
+			<button @click="make(true)" class="cu-btn round bg-green margin-top">
+				<text class="cuIcon-refresh margin-right-xs"></text>刷新
+			</button>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	import uQRCode from '@/uni_modules/Sansnn-uQRCode/components/uqrcode/common/uqrcode'
+
+	export default {
+		data() {
+			return {
+				qrcodeSize: 256,
+				code: '',
+				tips: '',
+			}
+		},
+		onLoad() {
+			// 在onReady中调用绘制二维码方法，尽量不要在onLoad中
+			const code = uni.getStorageSync('code')
+			if (!code) {
+				uni.redirectTo({ url: './setting' })
+				return
+			}
+			this.code = code
+		},
+		onReady() {
+			this.make()
+		},
+		onShow () {		
+			// 每分钟刷新
+			var _this = this
+			setInterval(function() {
+				let timestamp = Date.parse(new Date())
+				if (timestamp % (60 * 1000) == 0 && _this.code) {
+					_this.make()
+				}
+			}, 1000)
+		},
+		methods: {
+			make(isRefresh = false) {
+				uni.showLoading({
+					mask: true,
+					title: '生成中...'
+				})
+
+				const code = this.code
+				if (!code) {
+					uni.redirectTo({ url: './setting' })
+					uni.hideLoading()
+					return
+				}
+				let timestamp = Date.parse(new Date()).toString()
+				timestamp = timestamp.substr(0, 10)
+				timestamp = Number.parseInt(timestamp) + 60
+				const codeStr = {
+					sfmbm: code,
+					sfmyxjzsj: timestamp
+				}
+				// console.log('codeStr', codeStr)
+
+				this.$refs
+					.uqrcode
+					.make({
+						mode: 'canvas',
+						size: this.qrcodeSize,
+						text: JSON.stringify(codeStr),
+						margin: 10
+					})
+				const tipsDatetime = this.timestamp2Time(timestamp)
+				this.tips = `${tipsDatetime} 前有效`
+				uni.hideLoading()
+				if (isRefresh) {
+					uni.showToast({
+						title: '二维码已刷新'
+					})
+				}
+			},
+			go2Setting () {
+				uni.navigateTo({
+					url: './setting'
+				})
+			},
+			timestamp2Time(timestamp) {
+				var date = new Date(Number.parseInt(String(timestamp) + '000'))
+				var Y = date.getFullYear() + '-'
+				var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
+				var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' '
+				var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':'
+				var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':'
+				var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds())
+				const strDate = Y + M + D + h + m + s
+				return strDate
+			}
+		}
+	}
+</script>
+
+<style>
+	/* #ifndef APP-NVUE */
+	page {
+		background-color: #f0f0f0;
+	}
+
+	/* #endif */
+
+	.title {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		margin-top: 50rpx;
+		font-size: 36rpx;
+		color: #666666;
+	}
+
+	.qrcode-box,
+	.image-box,
+	.canvas-box {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		margin-top: 50rpx;
+	}
+
+	.text {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		margin-top: 12rpx;
+		font-size: 34rpx;
+	}
+
+	.image-box {
+		width: 400rpx;
+		margin-top: 50rpx;
+	}
+
+	.image {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		width: 400rpx;
+		height: 400rpx;
+	}
+
+	.input {
+		width: 600rpx;
+		height: 40px;
+		margin: 50rpx 0;
+		padding: 0 20rpx;
+		border: 1px solid #b0b0b0;
+		border-radius: 5px;
+		background-color: #ffffff;
+	}
+
+	.button {
+		width: 690rpx;
+		margin: 10rpx;
+	}
+
+	.button:last-child {
+		margin-bottom: 50rpx;
+	}
+
+	.component {
+		margin-top: 30rpx;
+		text-align: center;
+	}
+
+	.component-title {
+		/* #ifndef APP-NVUE */
+		display: inline-block;
+		/* #endif */
+		padding: 20rpx 40rpx;
+		border-bottom: 2px solid #d8d8d8;
+		font-size: 36rpx;
+	}
+
+	.component-buttons {
+		margin-top: 30rpx;
+	}
+</style>
---
+++ pages/index/setting.vue
@@ -0,0 +1,80 @@
+<template>
+	<view class="content">
+		<cu-custom bgColor="bg-gradual-blue" isGoHome="true">
+			<block slot="content">设置</block>
+		</cu-custom>
+
+		<form>
+			<view class="text-center">
+				<image style="height: 300px;" src="../../static/logo.png" mode="heightFix"></image>
+			</view>
+			<view class="cu-form-group radius margin-top margin-lr">
+				<textarea maxlength="-1" v-model="code" placeholder='请输入或扫描识别"e鹭安 - 个人平安码"'></textarea>
+			</view>
+		</form>
+		
+		<view class="padding flex flex-wrap justify-between margin-lr-xl align-center">
+			<button @click="scan()" class="cu-btn round bg-green"><text class="cuIcon-scan margin-right-xs"></text>扫描识别</button>
+			<button @click="save()" class="cu-btn round bg-green"><text class="cuIcon-check margin-right-xs"></text>确认保存</button>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				code: ''
+			}
+		},
+		onLoad() {
+			const code = uni.getStorageSync('code')
+			if (code) {
+				this.code = code
+			}
+		},
+		onReady() { },
+		methods: {
+			scan () {
+				var _this = this
+				uni.scanCode({
+					scanType: ['qrCode'],
+					success: function (res) {
+						const json = JSON.parse(res.result)
+						if (!json.sfmbm) {
+							uni.showToast({
+								title: '未识别到e鹭安个人平安码'
+							})
+							return
+						}
+						_this.code = json.sfmbm
+					}
+				})
+			},
+			save () {
+				if (!this.code) {
+					uni.showToast({
+						icon: 'none',
+						title: 'e鹭安平安码不得为空'
+					})
+					return
+				}
+				
+				uni.setStorageSync('code', this.code)
+				uni.showToast({
+					title: '已保存'
+				})
+				setTimeout(function(){
+					uni.redirectTo({
+						url: './index'
+					})
+				}, 1500)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
