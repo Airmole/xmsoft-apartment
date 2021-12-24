147a11f8df1c636f45d40498da1cadb199fe6d3b
Airmole <shixi_zhangbo3@staff.weibo.com>
2021-12-25 00:17:33 +0800

    优化权限&加快启动加载速度

--- App.vue
+++ App.vue
@@ -2,28 +2,36 @@
 	import Vue from 'vue'
 	export default {
 		onLaunch: function() {
-		  uni.getSystemInfo({
+			
+			// #ifdef APP-PLUS
+			//app关闭默认的启动 方法关闭启动图。但是这个时间不能太晚，6s 超时后依旧会主动关闭。
+			setTimeout(()=>{
+				plus.navigator.closeSplashscreen();
+			},100)
+			// #endif
+			
+			uni.getSystemInfo({
 			success: function(e) {
-			  // #ifndef MP
-			  Vue.prototype.StatusBar = e.statusBarHeight;
-			  if (e.platform == 'android') {
-				Vue.prototype.CustomBar = e.statusBarHeight + 50;
-			  } else {
-				Vue.prototype.CustomBar = e.statusBarHeight + 45;
-			  };
-			  // #endif
-			  // #ifdef MP-WEIXIN
-			  Vue.prototype.StatusBar = e.statusBarHeight;
-			  let custom = wx.getMenuButtonBoundingClientRect();
-			  Vue.prototype.Custom = custom;
-			  Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
-			  // #endif		
-			  // #ifdef MP-ALIPAY
-			  Vue.prototype.StatusBar = e.statusBarHeight;
-			  Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
-			  // #endif
-			}
-		  })
+				  // #ifndef MP
+				  Vue.prototype.StatusBar = e.statusBarHeight;
+				  if (e.platform == 'android') {
+					Vue.prototype.CustomBar = e.statusBarHeight + 50;
+				  } else {
+					Vue.prototype.CustomBar = e.statusBarHeight + 45;
+				  };
+				  // #endif
+				  // #ifdef MP-WEIXIN
+				  Vue.prototype.StatusBar = e.statusBarHeight;
+				  let custom = wx.getMenuButtonBoundingClientRect();
+				  Vue.prototype.Custom = custom;
+				  Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+				  // #endif		
+				  // #ifdef MP-ALIPAY
+				  Vue.prototype.StatusBar = e.statusBarHeight;
+				  Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
+				  // #endif
+				}
+			})
 		},
 		onShow: function() {
 			console.log('App Show')
